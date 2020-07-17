ab633996cbee0b466b086a9b654bd3e6add00d83
vitor.rocha <vitor.rocha@sgsistemas.com.br>
2020-07-17 10:35:12 -0300

    Novo projeto Spring

---
+++ Trainee/src/main/java/com/aulaJavaNova/Trainee/TraineeApplication.java
@@ -0,0 +1,13 @@
+package com.aulaJavaNova.Trainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeApplication.class, args);
+	}
+
+}
---
+++ Trainee/src/test/java/com/aulaJavaNova/Trainee/TraineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaJavaNova.Trainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
